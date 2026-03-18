e7c39d3255773b8195c04907fb89a67adeb1d4c9
shakoaws7341-design <shakoaws7341@gmail.com>
2026-03-18 10:42:59 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,25 @@
-resource "aws_launch_template" "app" {
-    name_prefix = "devops-template"
-    image_id = "ami-0f58b397bc5c1f2e8"
-    instance_type = "t2.micro"
-    key_name = var.key_name
-    user_data = filebase64("../app/install.sh")
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
 
-    network_interfaces {
-      associate_public_ip_address = false 
-      security_groups = [ aws_security_group.ec2_sg.id ]
-    }
-  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public1.id
+
+  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y apache2
+              systemctl start apache2
+              echo "Hello from EC2" > /var/www/html/index.html
+              EOF
 }
