8095f72d0e1b5a118d863f887f95c924a40174db
shakoaws7341-design <shakoaws7341@gmail.com>
2026-03-18 10:38:32 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,27 +1,30 @@
-resource "aws_alb" "alb" {
-    name = "devops-alb"
-    internal = false 
-    load_balancer_type = "application"
+resource "aws_lb" "alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
 
-    security_groups = [ aws_security_group.alb_sg.id ]
-    subnets = [ 
-        aws_subnet.public1,
-        aws_subnet.public2 ]
-  
+  security_groups = [aws_security_group.alb_sg.id]
+
+  subnets = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
 }
 
 resource "aws_lb_target_group" "tg" {
-  port = 80
+  name     = "my-tg"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = aws_vpc.main.id
+  vpc_id   = aws_vpc.main.id
 }
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port = 80
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
