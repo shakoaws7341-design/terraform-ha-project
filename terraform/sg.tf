611bea081b1ac716c371a801203362bde99566c8
shakoaws7341-design <shakoaws7341@gmail.com>
2026-03-18 10:41:57 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -6,7 +6,26 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # ✅ FIXED
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
