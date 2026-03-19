7de42e9434a5e2af1761a72e9ab9741451aa2653
sharique khan <shakoaws7341@gmail.com>
2026-03-19 05:47:01 +0530

    asg update

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,17 +1,35 @@
+resource "aws_launch_template" "app" {
+  name_prefix   = "app-lt"
+  image_id      = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y apache2
+              systemctl start apache2
+              echo "Hello from ASG" > /var/www/html/index.html
+              EOF
+  )
+}
 resource "aws_autoscaling_group" "asg" {
+  desired_capacity = 2
+  max_size         = 3
+  min_size         = 1
 
-    desired_capacity = 2
-    max_size = 3
-    min_size = 2
+  vpc_zone_identifier = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
 
-    vpc_zone_identifier = [ 
-        aws_subnet.private1,aws_subnet.private2
-     ]
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
 
-     target_group_arns = aws_lb_target_group.tg.arn
-    launch_template {
-      id = aws_launch_template.app.id
-      version = "$latest"
-    }
-  
+  target_group_arns = [aws_lb_target_group.tg.arn]
+
+  health_check_type = "EC2"
 }
