85f10844768be41be48c238cfad3640c088c3fd6
sharique khan <shakoaws7341@gmail.com>
2026-03-19 07:25:45 +0530

    final rote update

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "alb_sg" {
         from_port = 3306 
         to_port = 3306 
         protocol = "tcp" 
-        security_groups = [aws_security_group.ec2_sg.id] 
+        security_groups = [aws_security_group.alb_sg.id] 
         } 
         egress {
            from_port = 0 
