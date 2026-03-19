e62658de7cfe42fb648f6204ffbf4d87a8a991c3
sharique khan <shakoaws7341@gmail.com>
2026-03-19 05:34:57 +0530

    subnet change

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "private1" {
 
 resource "aws_subnet" "private2" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10..0.4.0/24"
+    cidr_block = "10.0.4.0/24"
     availability_zone = "ap-south-1b"
   
 }
