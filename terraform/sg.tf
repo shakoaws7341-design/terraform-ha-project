d1e27749d529dcac6276a6e771fea244ba359da7
sharique khan <shakoaws7341@gmail.com>
2026-03-19 06:59:30 +0530

    final rote update

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,30 +1,12 @@
-resource "aws_security_group" "alb_sg" {
-  name   = "alb-sg"
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
   vpc_id = aws_vpc.main.id
 
+  # ALB → EC2 (IMPORTANT)
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "rds_sg" {
-  name   = "rds-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
 
